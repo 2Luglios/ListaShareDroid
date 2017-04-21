aa7221fb9f65a41e4bce67da14178a30eaa780a1
Ettore Luglio <ettoreluglio@Tamadrum.local>
2017-04-21 15:24:04 -0300

    horizontallistview com views simples

--- app/src/main/java/br/com/a2luglios/listasharedroid/GruposActivity.java
+++ app/src/main/java/br/com/a2luglios/listasharedroid/GruposActivity.java
@@ -1,10 +1,15 @@
 package br.com.a2luglios.listasharedroid;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import br.com.a2luglios.listasharedroid.adapters.ListaListasAdapter;
 
@@ -22,5 +27,18 @@ public class GruposActivity extends AppCompatActivity {
                 startActivity(new Intent(GruposActivity.this, ListaListViewActivity.class));
             }
         });
+
+        LinearLayout listaGrupos = (LinearLayout)findViewById(R.id.listaGrupoInterna);
+        TextView tv = new TextView(this);
+        tv.setText("Texto");
+        tv.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        TextView tv2 = new TextView(this);
+        tv2.setText("Texto");
+        tv2.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        listaGrupos.addView(tv, 0);
+        listaGrupos.addView(tv2, 0);
+
     }
 }
